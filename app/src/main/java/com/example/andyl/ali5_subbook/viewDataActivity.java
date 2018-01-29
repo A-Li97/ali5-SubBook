1aac8333025b6dbba70bd7c0a4b3e55f0d962e0d
Andy Li <ali5@cs.ualberta.ca>
2018-01-28 23:40:17 -0700

    Added two buttons to create new intents

---
+++ app/src/main/java/com/example/andyl/ali5_subbook/viewDataActivity.java
@@ -0,0 +1,13 @@
+package com.example.andyl.ali5_subbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class viewDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_data);
+    }
+}
